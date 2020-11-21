16da7b517155a6b7f7831cfe9c9f905093834534
JocsanMoreno <71853949+JocsanMoreno@users.noreply.github.com>
2020-11-21 16:06:43 -0600

    Se explico y agregaron notas

--- introduction/area.c
+++ introduction/area.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-#include<math.h>
-#define PI 3.1416
+#include<stdio.h> //Imput Output
+#include<math.h> // al igual que stdio math es una biblioteca la cual tiene ya comandos
+#define PI 3.1416   //Definiendo una constante
 
-int main(){
-	float area, radio;
-	radio = 5;
-	area = PI * pow(5,2);
-	printf("Area\n");
-	printf("%s%f\n\n", "Area de Circulo con radio 5:", area);
+int main(){ //En la funcion int se debe tener un valor de regreso, es decir ingresas algo y tiene que regresar un valor entero , si no se quiere eso se usa la funcion Void
+	float area, radio;   //Declarando variables de tipo flotante
+	radio = 5;  //Asignando valor a variable radio 
+	area = PI * pow(5,2); //Asignando resultado de la operacion a variable area
+	printf("Area\n"); //Imprimiendo Titulo
+	// Imprimiendo resultado
+	printf("Area de Circulo con radio 5: %f", area);
+	return 0;
 }
--- introduction/hello.c
+++ introduction/hello.c
@@ -1,11 +1,12 @@
 #include<stdio.h> //stdio -> Standard Input/Output
 
-int main(){
+void main(){
 	/*My first
 	 * Program
 	 * in C
 	 */
+	 /*A veces siento que esto es dificil pero creo 
+	 que solo me ahogo dentro de un vaso de agua*/
+	  
 	printf("Hello World!");
-	//Comment
-	return 0;
 }
